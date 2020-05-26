3ef68c972f49d60a0c8cbf8e39ebe382485ab62c
zhaokuohaha <1014336691@qq.com>
2020-05-27 02:36:47 +0800

    添加npoi.mapper测试

---
+++ SimpleTests/NpoiTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Npoi.Mapper;
+using Npoi.Mapper.Attributes;
+using NPOI.SS.Formula.Functions;
+using System.Linq;
+using System.IO;
+
+namespace SimpleTests
+{
+	public class NpoiTest
+	{
+		[Fact]
+		public void TestNpoiPut()
+		{
+			var path = Path.Combine(Directory.GetCurrentDirectory(), "test.xlsx");
+			var u1 = new[] { new NUser { Name = "U1", Age = 12 } };
+			var u2 = new[] { new NUser { Name = "U2", Age = 13 } };
+
+			foreach(var userBatch in new[] { u1, u2 })
+			{
+				var mapper = new Mapper();
+				mapper.Save(path, userBatch, 0, !File.Exists(path));
+			}
+
+			var data = (new Mapper(path)).Take<NUser>();
+			Assert.Null(data.Last().Value.Name);
+
+		}
+	}
+
+	public class NUser
+	{
+		[Column("MyName")]
+		public string Name { get; set; }
+		public int Age { get; set; }
+	}
+}
---
+++ SimpleTests/TryCatchTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTests
+{
+	/// <summary>
+	/// 测试一下TryCatch的性能
+	/// </summary>
+	public class TryCatchTest
+	{
+        public void TryCatchTest1(string[] args)
+        {
+            int j = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                try
+                {
+                    j = i;
+                    throw new System.Exception();
+                }
+                catch { }
+            }
+            System.Console.Write(j);
+            return;
+        }
+	}
+}
